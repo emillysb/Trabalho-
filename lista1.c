4e2b2d15db5ebefa1b1b97df434ac2b0b7009f89
emillysb <emillybastos2022@gmail.com>
2024-11-02 00:30:04 +0000

    lista1

--- lista1.c
+++ lista1.c
@@ -127,14 +127,49 @@ void q12() {
 
 //13. Faça um programa que leia o saldo de uma conta poupança e
 //    imprima o novo saldo, considerando um reajuste de 2%.
+void q13() {
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void main()
+    {
+        float saldo, novoSaldo;
+        printf("Digite o saldo:");
+        scanf("%f", &saldo);
+        novoSaldo = saldo + saldo * 0.02;
+        printf("saldo com reajuste: %f", novoSaldo);
+    }
+}
 
 //14. Faça um programa que leia a base e a altura de um retângulo
 //    e imprima o perímetro (base*2 + altura*2) e a área (base * altura).
+void q14() {
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    void main()
+    float base, altura, perimetro, area;
+    printf ("digite a base:");
+    scanf("%f", &altura);
+    perimetro = base+altura;
+    area = base*altura;
+    printf (perimetro:%if\n", perimetro);
+    print (area:%f\n", area);   
+}
 
 //15. Faça um programa que leia o valor de um produto, o percentual
 //    do desconto desejado e imprima o valor do desconto e o valor
 //    do produto subtraindo o desconto.
+void q15 () {
+    include <stdio.h>
+    include <stdlib.h>
 
+    void main()
+    float vlrProd, percDesc, vlrDesc;
+    printf("Digite o valor do produto:");
+    scanf("%f, &vlrProd");
+    printf("Digite o desconto:");
+}
 //16. Faça um programa que calcule o reajuste do salário de um
 //    funcionário. Para isso, o programa deverá ler o salário atual
 //    do funcionário e ler o percentual de reajuste. Ao final imprimir
@@ -183,7 +218,9 @@ int main() {
     q10();
     q11();
     q12();
-
+    q13();
+    q14();
+    q15();
 
 
 
